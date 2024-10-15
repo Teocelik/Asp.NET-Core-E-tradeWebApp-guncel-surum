992eb1663a06d3c73a464a0fcd3e16b6dce09f26
Teocelik <aalihan@outlook.com.tr>
2024-10-15 22:11:33 +0300

    SessionExtension sınıfı eklendi. Serilizasyon ve deserilizasyon işlemleri için SessionExtension sınıfına methodlar eklendi.

---
+++ Extensions/SessionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace KendinInşaEtSonSurumWebApp.Extensions
+{
+    //Extension(Uzatma sınıflar) class'lar mevcut sınıfa dokunmadan yeni özellikler eklememizi sağlar.
+    // this anahtar kelimesi ile belirttiğimiz türe özel genişletmeler yapabiliyoruz.
+    // Burada, ISession arabirimine bir nesneyi Json formatında saklama özelliği ekleyerek genişlettim.
+    // Bu sayede, bir nesneyi Json formatına çevirerek oturumda saklayabilirim.
+    //Ayrıca bu nesneyi tekrar orjinal hale getirebilirim
+    public static class SessionExtensions
+    {
+        public static void SetObjectFromJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        // T? daki "?" işaret T türünün null olabileceğini belirtir. Ayrıca olası Null uyarılarının önüne geçmiş oluyor. 
+        public static T? GetObjectFromJson<T>(this ISession session, string key) 
+        {
+            var value = session.GetString(key);
+            return value == null ? default : JsonSerializer.Deserialize<T>(value);
+        }
+    }
+}
